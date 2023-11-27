250306f651c9cb4d0186af09ec15744376dd3d88
jrfm <moesch.jonas@protonmail.com>
2023-11-27 22:38:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1> hello world!</h1>
+    </>
+  );
+}
+
+export default App;
