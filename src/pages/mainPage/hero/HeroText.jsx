bb930fbd2aff3a39e108b0468d477833ac82f18c
jrfm <moesch.jonas@protonmail.com>
2023-12-22 19:40:17 +0100

    Change description of projects

--- src/pages/mainPage/hero/HeroText.jsx
+++ src/pages/mainPage/hero/HeroText.jsx
@@ -1,14 +1,14 @@
-//Text that is used in the about me section
+/**
+ * English version of text that is used in the hero section
+ */
 let aboutMeEng =
   "Hi, my name is Jonas. I am a munich based structural engineer who is fascinated by digital technologies. After finishing my Master's degree in civil engineering I started to learn programming. My aim was to combine traditional techniques used in civil engineering with the possibilities of building information modeling. Besides learning and applying new software solutions I wanted to be able to write my own programs and to extend existing software with new functionality.";
 
-// let aboutMe =
-//   "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-
-//
-
+/**
+ * German version of text that is used in the hero section
+ */
 let aboutMeGer =
-  "Hi, mein Name ist Jonas und ich bin ein in München lebender Tragwerksplaner / Statiker. Nach meinem Studium habe ich begonnen programmieren zu lernen. Mein Ziel war es, die nötigen Kenntnisse zu erwerben, um in der Lage zu sein, eigene Programme zu schreiben  beziehungsweise bestehende Programme mit neuer Funktionalität zu erweitern. Ich möchte mit meinen erworbenen Kentnisse die Digitalisierung der Baubranche aktiv mitgestalten";
+  "Hi, mein Name ist Jonas und ich bin ein in München lebender Tragwerksplaner / Statiker. Nach meinem Studium habe ich begonnen programmieren zu lernen. Mein Ziel war es, die nötigen Kenntnisse zu erwerben, um in der Lage zu sein, eigene Programme zu schreiben  beziehungsweise bestehende Programme mit neuer Funktionalität zu erweitern. Ich möchte mit meinen erworbenen Kentnisse die Digitalisierung der Baubranche aktiv mitgestalten.";
 
 let aboutMeGer2 =
   "Hi, ich heiße Jonas. Ich bin Tragwerksplaner und fasziniert von digitalen Technologien. Nach meinem Bauingeniuerstudium habe ich begonnen programmieren zu lernen. Mein Ziel ist es nervige Aufgaben zu automatisieren, um mehr Zeit für interessante Fragestellungen zu haben. Ich möchte neue Technologien mit bewährten Planungsprinzipien kombinieren um aktiver teil des digitalen Wandels sein zu können. ";
--- src/pages/mainPage/mainProjects/MainProjectData.jsx
+++ src/pages/mainPage/mainProjects/MainProjectData.jsx
@@ -6,7 +6,7 @@ const projectData = [
     tools: "Javascript, React.js, MUI, Vite",
     imgPath: "images/img_bemessungshilfen.png",
     description:
-      " A front end calculation tool for concrete construction. You can calculate the needed anchorage length of a rebar and the amount of rebars that can fit in a concrete beam.",
+      "Mit der Verankerungslänge-App lässt sich die Ersatzverankerungslänge nach EC2 berechnen. Die verwendenten Formeln, so wie die benötigten Beiwerte sind übersichtlich dargestellt. Das Berechnungsprotokoll dient als Dokumentation. Die einzelnen Rechenschritte können dadruch nachvollzogen werden. Es handelt sich bei der App um eine Front-End-Applikaiton.",
     id: uniqid(),
     link: "#",
   },
@@ -15,7 +15,7 @@ const projectData = [
     tools: "Javascript, React.js, MUI, Vite",
     imgPath: "images/img_bemessungshilfen.png",
     description:
-      "Mittige Längsdruckkraft A front end calculation tool for concrete construction. You can calculate the needed anchorage length of a rebar and the amount of rebars that can fit in a concrete beam.",
+      " Das Ziel war es eine App zu entwickeln, mit der man die Ersatzverankerungslänge nach EC2 berechnen kann. Mittige Längsdruckkraft A front end calculation tool for concrete construction. You can calculate the needed anchorage length of a rebar and the amount of rebars that can fit in a concrete beam.",
     id: uniqid(),
     link: "https://phenomenal-cheesecake-339368.netlify.app/",
   },
@@ -25,9 +25,22 @@ const projectData = [
     tools: "Javascript, React.js, MUI, Vite",
     imgPath: "images/img_bemessungshilfen.png",
     description:
-      " Anzahl Bewehrungsstäbe front end calculation tool for concrete construction. You can calculate the needed anchorage length of a rebar and the amount of rebars that can fit in a concrete beam.",
+      "Mit der App lässt sich die maximale Anzahl von Bewehrungsstäben in einer Lage eines Stahlbetonträgers ermitteln. Es wurde der Ansatz aus Bewehren von Stahlbetonbauwerken nach DIN EN 1992-1-1 mit naitonalem Anhang verwendet herausgegeben vom Institut für Stahlbetonbewehrung (ISB) e. V. ",
     id: uniqid(),
   },
 ];
 
 export { projectData };
+
+/**
+ * "Mit der App lässt sich die maximale Anzahl von Bewehrungsstäben
+ * in einer Lage eines Stahlbetonträgers ermitteln. Ziel war es ein Interface
+ * zu entwickeln, welches übersichtlich und wiederverwendbar ist. Es sollte in
+ * weiteren Projekten als Vorlage verwendet werden. Dabei gab es zwei
+ * grundlegende Herausforderungen. Es musste ein Interface-Layout gefunden
+ * werden welches übersichtlich ist, aber auch die Darstellung von
+ * unterschiedlichsten Infromationen erlaubt. Durch das verwenden von
+ * einklappbaren Layout-Elementen konnte dies erreicht werden.
+ * Die zweite Herausforderung war die Darstellung von Zahlen uns mathematischen
+ * Symbolen.",
+ */
