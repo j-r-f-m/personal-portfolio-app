813d1f197e64c49336f78f4fdd54c74ceada08d2
jrfm <moesch.jonas@protonmail.com>
2024-02-20 22:30:21 +0100

    Change hero text

--- src/pages/mainPage/hero/HeroText.jsx
+++ src/pages/mainPage/hero/HeroText.jsx
@@ -8,7 +8,7 @@ let aboutMeEng =
  * German version of text that is used in the hero section
  */
 let aboutMeGer =
-  "Hallo, ich bin Jonas, ein Tragwerksplaner/Statiker aus München. Nach meinem Studium habe ich mich dem Erlernen der Programmierung gewidmet. Mein Ziel war es, die erforderlichen Fähigkeiten zu erwerben, um selbstständig Programme zu entwickeln und bestehende Software um neue Funktionen zu erweitern. Insbesondere interessiere ich mich für die Automatisierung der Planerstellung in der Ausführungsplanung. Mit meinem erlangten Wissen strebe ich an, aktiv zur Digitalisierung der Baubranche beizutragen.";
+  "Hallo, ich bin Jonas, ein Tragwerksplaner aus München. Ich lerne programmieren, um die erforderlichen Fähigkeiten zu erwerben, um selbstständig Programme zu entwickeln und bestehende Software um neue Funktionen zu erweitern. Insbesondere interessiere ich mich für die Automatisierung der Planerstellung in der Ausführungsplanung. Mit meinem erlangten Wissen strebe ich an, aktiv zur Digitalisierung der Baubranche beizutragen.";
 /* let aboutMeGer2 =
   "Hi, ich heiße Jonas. Ich bin Tragwerksplaner und fasziniert von digitalen Technologien. Nach meinem Bauingeniuerstudium habe ich begonnen programmieren zu lernen. Mein Ziel ist es nervige Aufgaben zu automatisieren, um mehr Zeit für interessante Fragestellungen zu haben. Ich möchte neue Technologien mit bewährten Planungsprinzipien kombinieren um aktiver teil des digitalen Wandels sein zu können. ";
  */
